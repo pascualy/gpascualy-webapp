31eebca33404ff984ad3c8da45cab3f44321c725
Gabriel Pascualy <pascualy@umich.edu>
2015-04-04 18:06:40 -0400

    first commit: framework generated with yo angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gpascualyWebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gpascualyWebappApp
+ */
+angular.module('gpascualyWebappApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
